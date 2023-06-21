238b2b88e4bb8b6a268ea1070fd60e3f1ce57dae
Christoph Leuker <cleuker87@gmail.com>
2023-06-21 12:19:55 +0200

    add create function, start with edit

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,5 +1,5 @@
-import dbConnect from "../../../db/connect.js";
-import Place from "../../../db/models/Places.js";
+import dbConnect from "../../../../db/connect.js";
+import Place from "../../../../db/models/Places.js";
 
 export default async function handler(request, response) {
   await dbConnect();
@@ -12,6 +12,12 @@ export default async function handler(request, response) {
       return response.status(404).json({ status: "Not Found" });
     }
 
-    response.status(200).json(product);
+    response.status(200).json(place);
+  }
+
+  if (request.method === "PATCH") {
+    const updatedPlace = request.body;
+    await Place.findByIdAndUpdate(id, updatedPlace);
+    response.status(20).json({ status: "Place successfully updated" });
   }
 }
--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -11,4 +11,16 @@ export default async function handler(request, response) {
     }
     response.status(200).json(place);
   }
+
+  if (request.method === "POST") {
+    try {
+      const placeData = request.body;
+      await Place.create(placeData);
+
+      response.status(201).json({ status: "place createt" });
+    } catch (error) {
+      console.log(error);
+      response.status(400).json({ error: error.message });
+    }
+  }
 }
