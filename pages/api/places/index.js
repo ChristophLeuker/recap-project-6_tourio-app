20c56dfe8cf89bbda4be6c64c3d9a22a1861e0fc
Christoph Leuker <cleuker87@gmail.com>
2023-06-21 11:48:21 +0200

    add env

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,5 +1,14 @@
-import { places } from '../../../lib/db';
+import dbConnect from "../../../db/connect.js";
+import Place from "../../../db/models/Places.js";
 
-export default function handler(request, response) {
-  return response.status(200).json(places);
+export default async function handler(request, response) {
+  await dbConnect();
+  if (request.method === "GET") {
+    const place = await Place.find();
+
+    if (!place) {
+      return response.status(404).json({ status: "Not Found" });
+    }
+    response.status(200).json(place);
+  }
 }
